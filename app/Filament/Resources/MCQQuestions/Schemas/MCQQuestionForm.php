4c77a7dc1f29897fcd0eac5c819bef3d1d17178c
Md Jasim Uddin <mdjasimh944@gmail.com>
2026-02-04 21:23:09 +0600

    mcq question form backup

--- app/Filament/Resources/MCQQuestions/Schemas/MCQQuestionForm.php
+++ app/Filament/Resources/MCQQuestions/Schemas/MCQQuestionForm.php
@@ -2,47 +2,95 @@
 
 namespace App\Filament\Resources\MCQQuestions\Schemas;
 
-use App\Models\User;
 use App\Models\Lesson;
 use App\Models\Chapter;
 use App\Models\Subject;
 use Filament\Schemas\Schema;
 use App\Models\AcademicClass;
-use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+use Filament\Schemas\Components\Utilities\Get;
+use Filament\Schemas\Components\Utilities\Set;
 
 class MCQQuestionForm
 {
     public static function configure(Schema $schema): Schema
     {
         return $schema
+            ->columns(4)
             ->components([
                 Select::make('class_id')
                     ->label('Class')
                     ->required()
-                    ->options(AcademicClass::query()->pluck('name', 'id')),
+                    ->options(AcademicClass::query()->pluck('name', 'id'))
+                    ->preload()
+                    ->live()
+                    ->afterStateUpdated(function(Set $set){
+                        $set('subject_id', null);
+                        $set('chapter_id', null);
+                        $set('lesson_id', null);
+                    }),
 
                 Select::make('subject_id')
                     ->label('Subject')
                     ->required()
-                    ->options(Subject::query()->pluck('sub_name', 'id')),
+                    ->options(fn(Get $get) => Subject::query()
+                        ->where('class_id', $get('class_id'))
+                        ->pluck('sub_name', 'id'))
+                    ->preload()
+                    ->live()
+                    ->disabled(fn(Get $get)=>blank($get('class_id')))
+                    ->afterStateUpdated(function(Set $set){
+                        $set('chapter_id', null);
+                        $set('lesson_id', null);
+                    }),
 
 
                 Select::make('chapter_id')
                     ->label('Chapter')
-                    ->options(Chapter::query()->pluck('chapter_name', 'id')),
+                    ->options(fn(Get $get)=>Chapter::query()
+                        ->where('subject_id',$get('subject_id'))
+                        ->pluck('chapter_name', 'id'))
+                    ->preload()
+                    ->live()
+                    ->disabled(fn(Get $get)=>blank($get('subject_id')))
+                    ->afterStateUpdated(function(Set $set){
+                        $set('lesson_id', null);
+                    })
+                    ->nullable(),
 
                 Select::make('lesson_id')
                     ->label('Lesson')
-                    ->options(Lesson::query()->pluck('lesson_name', 'id')),
+                    ->options(fn(Get $get)=>Lesson::query()
+                        ->where('chapter_id', $get('chapter_id'))
+                        ->pluck('lesson_name', 'id'))
+                    ->preload()
+                    ->live()
+                    ->disabled(fn(Get $get)=> blank($get('chapter_id')))
+                    ->nullable(),
 
-                TextInput::make('questions')->label('Quesiton')->placeholder('Write Question')->required(),
+                FileUpload::make('proviking_img')
+                    ->label('Question Image')
+                    ->image()
+                    ->maxSize(3072)
+                    ->directory('proviking_images')
+                    ->nullable()
+                    ->columnSpan(1),
+
+                TextInput::make('questions')->label('Quesiton')->placeholder('Write Question')->required()->columnSpan(3),
+              
                 TextInput::make('option_a')->label('Option A')->placeholder('Option A')->required(),
                 TextInput::make('option_b')->label('Option B')->placeholder('Option B')->required(),
                 TextInput::make('option_c')->label('Option C')->placeholder('Option C')->required(),
                 TextInput::make('option_d')->label('Option D')->placeholder('Option D'),
                 TextInput::make('right_answer')->label('Right Answer')->placeholder('Right Answer')->required(),
+
+                TextInput::make('marks')
+                    ->label('Marks')
+                    ->numeric()
+                    ->required()
+                    ->default(1),
 
                 Select::make('level')
                     ->label('Level')
@@ -60,7 +108,16 @@ class MCQQuestionForm
                         'model_question' => 'Model Quesiton',
                         'custom_question' => 'Custom Question'
                     ]),
-                TextInput::make('year')->numeric()->required(),
+                TextInput::make('board_name')
+                    ->label('Board Name')
+                    ->disabled(fn(Get $get)=> $get('type') != 'board_question')
+                    ->required(fn(Get $get)=> $get('type') == 'board_question'),
+
+                TextInput::make('year')
+                    ->label('Year')
+                    ->numeric()
+                    ->required()
+                    ->default(now()->year),
                 
             ]);
     }
